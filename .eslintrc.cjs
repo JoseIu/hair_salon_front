e56e7277054d6c314070b8cd22546576596b033a
Joselu <joseluissgz97@gmail.com>
2024-07-22 09:43:20 +0200

    chore: Update eslint configuration t

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,16 +3,14 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
-}
+};
